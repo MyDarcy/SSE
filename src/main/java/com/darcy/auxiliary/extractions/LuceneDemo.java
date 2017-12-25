6c512de97d3ce43073105967486e70cd4af4b351
MyDarcy <darcy.q.cs@gmail.com>
2017-12-25 22:15:48 +0800

    tf-idf another version

---
+++ src/main/java/com/darcy/auxiliary/extractions/LuceneDemo.java
@@ -0,0 +1,126 @@
+package com.darcy.auxiliary.extractions;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * author: darcy
+ * date: 2017/11/9 22:50
+ * description:
+ */
+public class LuceneDemo {
+
+	public static void test() {
+		try {
+			// 要处理的文本
+			// "lucene分析器使用分词器和过滤器构成一个“管道”，文本在流经这个管道后成为可以进入索引的最小单位，
+			// 因此，一个标准的分析器有两个部分组成，一个是分词器tokenizer,它用于将文本按照规则切分为一个个可
+			// 以进入索引的最小单位。另外一个是TokenFilter，它主要作用是对切出来的词进行进一步的处理（如去掉敏
+			// 感词、英文大小写转换、单复数处理）等。lucene中的Tokenstram方法首先创建一个tokenizer对象处理Reader
+			// 对象中的流式文本，然后利用TokenFilter对输出流进行过滤处理";
+			String text = "The Lucene PMC is pleased to announce the release of the Apache Solr " +
+					"Reference Guide for Solr 7.1.0.";
+
+			// 自定义停用词
+			String[] self_stop_words = {"Lucene", "release", "Apache"};
+			CharArraySet cas = new CharArraySet(1, true);
+
+			for (int i = 0; i < self_stop_words.length; i++) {
+				cas.add(self_stop_words[i]);
+			}
+
+			// 加入系统默认停用词
+			Iterator<Object> itor = StandardAnalyzer.STOP_WORDS_SET.iterator();
+			while (itor.hasNext()) {
+				cas.add(itor.next());
+			}
+
+			// 标准分词器(Lucene内置的标准分析器,会将语汇单元转成小写形式，并去除停用词及标点符号)
+			StandardAnalyzer sa = new StandardAnalyzer();
+
+			TokenStream ts = sa.tokenStream("field", text);
+			CharTermAttribute ch = ts.addAttribute(CharTermAttribute.class);
+
+			ts.reset();
+			while (ts.incrementToken()) {
+				System.out.println(ch.toString());
+			}
+			ts.end();
+			ts.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public static void test2() throws IOException {
+		try {
+			File file = new File("D:\\MrDarcy\\ForGraduationWorks\\Code\\SSE\\doc\\base\\plain\\f00004.txt");
+			Reader reader = new FileReader(file);
+			Analyzer a = new StandardAnalyzer();
+			//Analyzer a = new CJKAnalyzer();
+			//Analyzer a = new ChineseAnalyzer();
+			//Analyzer a = new WhitespaceAnalyzer();
+			TokenStream ts = a.tokenStream("", reader);
+			int n = 0;
+
+			ts.reset();
+			while (ts.incrementToken()) {
+				n++;
+				System.out.println("词条" + n + "的内容为 ：" + ts.toString());
+			}
+			ts.end();
+			ts.close();
+			System.out.println("== 共有词条 " + n + " 条 ==");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void test3() {
+		StandardAnalyzer analyzer = new StandardAnalyzer();
+
+		try {
+			List<String> lines = Files.readAllLines(new File("D:\\MrDarcy\\ForGraduationWorks\\Code\\SSE\\doc\\base\\plain\\f00004.txt").toPath());
+			for (String line : lines) {
+				TokenStream tokenStream = analyzer.tokenStream("field", line);
+				tokenStream.reset();
+				CharTermAttribute ch = tokenStream.addAttribute(CharTermAttribute.class);
+
+				while (tokenStream.incrementToken()) {
+					System.out.println(ch.toString());
+				}
+				tokenStream.end();
+				tokenStream.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		StandardAnalyzer sa = new StandardAnalyzer();
+
+		test();
+
+		System.out.println();
+
+
+		test2();
+
+		System.out.println();
+
+		test3();
+	}
+
+}
---
+++ src/main/java/com/darcy/auxiliary/extractions/Utilities.java
@@ -0,0 +1,177 @@
+package com.darcy.auxiliary.extractions;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.miscellaneous.ASCIIFoldingFilter;
+import org.apache.lucene.analysis.standard.ClassicFilter;
+import org.apache.lucene.analysis.standard.ClassicTokenizer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.TermFrequencyAttribute;
+import org.apache.lucene.analysis.tokenattributes.TermFrequencyAttributeImpl;
+import org.apache.lucene.util.AttributeFactory;
+import org.apache.lucene.util.Version;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.*;
+
+/*
+ * author: darcy
+ * date: 2017/12/25 9:48
+ * description: 
+*/
+public class Utilities {
+	public static String stem(String term) throws IOException {
+
+		TokenStream tokenStream = null;
+		try {
+
+			// tokenize
+			// tokenStream = new ClassicTokenizer(Version.LUCENE_7_1_0, new StringReader(term));
+			Analyzer a = new StandardAnalyzer();
+			// TokenStream ts = a.tokenStream("", term);
+			tokenStream = a.tokenStream("", term);
+
+			// stem
+			tokenStream = new PorterStemFilter(tokenStream);
+
+			// add each token in a set, so that duplicates are removed
+			Set<String> stems = new HashSet<String>();
+			CharTermAttribute token = tokenStream.getAttribute(CharTermAttribute.class);
+			tokenStream.reset();
+			while (tokenStream.incrementToken()) {
+				stems.add(token.toString());
+			}
+			tokenStream.end();
+			tokenStream.close();
+
+			// if no stem or 2+ stems have been found, return null
+			if (stems.size() != 1) {
+				return null;
+			}
+			String stem = stems.iterator().next();
+			// if the stem has non-alphanumerical chars, return null
+			if (!stem.matches("[a-zA-Z0-9-]+")) {
+				return null;
+			}
+
+			return stem;
+
+		} finally {
+			if (tokenStream != null) {
+				tokenStream.close();
+			}
+		}
+
+	}
+
+	public static <T> T find(Collection<T> collection, T example) {
+		for (T element : collection) {
+			if (element.equals(example)) {
+				return element;
+			}
+		}
+		collection.add(example);
+		return example;
+	}
+
+	public static List<Keyword> guessFromString(String input) throws IOException {
+
+		TokenStream tokenStream = null;
+		try {
+
+			/*// hack to keep dashed words (e.g. "non-specific" rather than "non" and "specific")
+			input = input.replaceAll("-+", "-0");
+			// replace any punctuation char but apostrophes and dashes by a space
+			input = input.replaceAll("[\\p{Punct}&&[^'-]]+", " ");
+			// replace most common english contractions
+			input = input.replaceAll("(?:'(?:[tdsm]|[vr]e|ll))+\\b", "");*/
+
+			// tokenize input
+			// tokenStream = new ClassicTokenizer(Version.LUCENE_36, new StringReader(input));
+			// ClassicTokenizer was named StandardTokenizer in Lucene versions prior to 3.1
+
+			Analyzer a = new StandardAnalyzer();
+
+			tokenStream = a.tokenStream(null, input);
+			// to lowercase
+			tokenStream = new LowerCaseFilter(tokenStream);
+			// remove dots from acronyms (and "'s" but already done manually above)
+			tokenStream = new ClassicFilter(tokenStream);
+			// convert any char to ASCII
+			tokenStream = new ASCIIFoldingFilter(tokenStream);
+			// remove english stop words
+			// tokenStream = new StopFilter(Version.LUCENE_7_1_0, tokenStream, EnglishAnalyzer.getDefaultStopSet());
+			tokenStream = new StopFilter(tokenStream, EnglishAnalyzer.getDefaultStopSet());
+
+			List<Keyword> keywords = new LinkedList<Keyword>();
+			CharTermAttribute token = tokenStream.getAttribute(CharTermAttribute.class);
+
+			tokenStream.reset();
+			while (tokenStream.incrementToken()) {
+				String term = token.toString();
+				System.out.println(term);
+				// stem each term
+
+				String stem = stem(term);
+				if (stem != null) {
+					// create the keyword or get the existing one if any
+					Keyword keyword = find(keywords, new Keyword(stem.replaceAll("-0", "-")));
+					// add its corresponding initial token
+					keyword.add(term.replaceAll("-0", "-"));
+				}
+			}
+			tokenStream.end();
+			tokenStream.close();
+
+			// reverse sort by frequency
+			Collections.sort(keywords);
+
+			return keywords;
+
+		} finally {
+			if (tokenStream != null) {
+				tokenStream.close();
+			}
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		String input = "Java is a general-purpose computer programming language that is concurrent, " +
+				"class-based, object-oriented,[15] and specifically designed to have as few implementation " +
+				"dependencies as possible. It is intended to let application developers \"write once, run anywhere\" " +
+				"(WORA),[16] meaning that compiled Java code can run on all platforms that support Java without the " +
+				"need for recompilation.[17] Java applications are typically compiled to bytecode that can run on " +
+				"any Java virtual machine (JVM) regardless of computer architecture. As of 2016, Java is one of the " +
+				"most popular programming languages in use,[18][19][20][21] particularly for client-server web " +
+				"applications, with a reported 9 million developers.[22] Java was originally developed by James " +
+				"Gosling at Sun Microsystems (which has since been acquired by Oracle Corporation) and released " +
+				"in 1995 as a core component of Sun Microsystems' Java platform. The language derives much of its " +
+				"syntax from C and C++, but it has fewer low-level facilities than either of them.\n" +
+				"\n" +
+				"The original and reference implementation Java compilers, virtual machines, and class libraries " +
+				"were originally released by Sun under proprietary licenses. As of May 2007, in compliance with the " +
+				"specifications of the Java Community Process, Sun relicensed most of its Java technologies under " +
+				"the GNU General Public License. Others have also developed alternative implementations of these " +
+				"Sun technologies, such as the GNU Compiler for Java (bytecode compiler), GNU Classpath (standard " +
+				"libraries), and IcedTea-Web (browser plugin for applets).\n" +
+				"\n" +
+				"The latest version is Java 9, released on September 21, 2017,[23] and is one of the two versions " +
+				"currently supported for free by Oracle. Versions earlier than Java 8 are supported by companies " +
+				"on a commercial basis; e.g. by Oracle back to Java 6 as of October 2017 (while they still \"highly " +
+				"recommend that you uninstall\"[citation needed] pre-Java 8 from at least Windows computers).";
+
+		List<Keyword> keywords = guessFromString(input);
+
+
+	}
+
+
+}
